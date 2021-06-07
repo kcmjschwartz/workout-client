7c8caf34ab6b51393f5aaccdc929968be36fcb42
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-07 17:04:35 -0400

    login and signup error fixed

---
+++ src/workouts/WorkoutIndex.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import {Container, Row, Col} from 'reactstrap';
+import WorkoutCreate from './WorkoutCreate';
+
+const WorkoutIndex = (props) => {
+    const [workouts, setWorkouts] = useState([]);
+
+    const fetchWorkouts = () => {
+        fetch('http://localhost:4000/log', {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type' : 'application/json',
+                'Authentication' : props.token
+            })
+        }) .then ((res) => res.json())
+        .then ((logData) => {
+            setWorkouts(logData)
+            console.log(logData);
+        })
+    }
+
+    useEffect(() =>{
+        fetchWorkouts();
+    }, [])
+
+    return(
+    <Container>
+        <Row>
+            <Col md="3">
+                <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
+            </Col>
+            <Col md="9">
+                <h2>Log a workout to see a table.  This will be added in later pages.</h2>
+            </Col>
+        </Row>
+    </Container>
+    )
+}
+
+export default WorkoutIndex;
